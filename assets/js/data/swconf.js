734df635ad98eddf3d87d1b10b2e58c742d69086
root <root@debian>
2024-10-07 10:21:13 +0200

    Actualización del sitio desplegado lun 07 oct 2024 10:21:13 CEST

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728288992',resources: [
+    cacheName: 'chirpy-1728289267',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
