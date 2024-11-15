7490fad6a29c984b8423b0f91659d5f74e7e86ef
Pablo Martín Hidalgo <pmartinhidalgo19@gmail.com>
2024-11-15 18:45:46 +0100

    chore: Actualización del sitio desplegado vie 15 nov 2024 18:45:46 CET

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731692708',resources: [
+    cacheName: 'chirpy-1731692746',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
